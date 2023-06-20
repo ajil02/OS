73a05eba93b6671e5bb50bfab245acefa43db39e
Ajil <94738733+ajil02@users.noreply.github.com>
2023-06-20 10:34:36 +0530

    Update message_queue.c

--- Message_Queue/message_queue.c
+++ Message_Queue/message_queue.c
@@ -1,26 +1,3 @@
-//TO READ DATA
-
-#include<stdio.h>
-#include<sys/ipc.h>
-#include<sys/msg.h>
-
-struct msg_buffer
-{
-	long msg_type;
-	char msg_text[100];
-}message;
-
-void main()
-{
-	key_t key;
-	int msgid;
-	key=ftok("progfile",65);
-	msgid=msgget(key,0666|IPC_CREAT);
-	msgrcv(msgid,&message,sizeof(message),1,0);
-	printf("The data recieved is:%s\n",message.msg_text);
-	msgctl(msgid,IPC_RMID,NULL);
-}
-
 //TO WRITE DATA
 
 #include<stdio.h>
@@ -46,3 +23,28 @@ void main()
 	msgsnd(msgid,&message,sizeof(message),0);
 	printf("Data send is:%s\n",message.msg_text);
 }
+
+
+//TO READ DATA
+
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+
+struct msg_buffer
+{
+	long msg_type;
+	char msg_text[100];
+}message;
+
+void main()
+{
+	key_t key;
+	int msgid;
+	key=ftok("progfile",65);
+	msgid=msgget(key,0666|IPC_CREAT);
+	msgrcv(msgid,&message,sizeof(message),1,0);
+	printf("The data recieved is:%s\n",message.msg_text);
+	msgctl(msgid,IPC_RMID,NULL);
+}
+
