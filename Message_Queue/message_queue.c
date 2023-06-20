4d3208cc534a366f7b628a542a59e6501480a8a5
Ajil <94738733+ajil02@users.noreply.github.com>
2023-06-20 10:31:44 +0530

    Create message_queue.c

---
+++ Message_Queue/message_queue.c
@@ -0,0 +1,48 @@
+//TO READ DATA
+
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+
+struct msg_buffer
+{
+	long msg_type;
+	char msg_text[100];
+}message;
+
+void main()
+{
+	key_t key;
+	int msgid;
+	key=ftok("progfile",65);
+	msgid=msgget(key,0666|IPC_CREAT);
+	msgrcv(msgid,&message,sizeof(message),1,0);
+	printf("The data recieved is:%s\n",message.msg_text);
+	msgctl(msgid,IPC_RMID,NULL);
+}
+
+//TO WRITE DATA
+
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#define MAX 100
+
+struct msg_buffer
+{
+	long msg_type;
+	char msg_text[100];
+}message;
+
+void main()
+{
+	key_t key;
+	int msgid;
+	key=ftok("progfile",65);
+	msgid=msgget(key,0666|IPC_CREAT);
+	message.msg_type=1;
+	printf("Write data to be read:");
+	fgets(message.msg_text,MAX,stdin);
+	msgsnd(msgid,&message,sizeof(message),0);
+	printf("Data send is:%s\n",message.msg_text);
+}
